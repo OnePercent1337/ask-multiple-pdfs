aba92645d98b9de651dfb98128fd2369351b1520
OnePercent1337 <55022557+OnePercent1337@users.noreply.github.com>
2023-08-27 21:34:02 +0200

    Test

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+import openai
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -32,14 +33,43 @@ def get_text_chunks(text):
 
 def get_vectorstore(text_chunks):
     embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
+def handle_userinput(user_question):
+    # Hardcoded responses for specific queries
+    if user_question.lower() == "who are you?":
+        assistant_reply = "I am a chatbot created by ChainHub.io to assist with crypto-related questions."
+    else:
+        # Existing logic here...
+        conversation = [
+            {"system": "You are a specialized chatbot created by ChainHub.io. You should only refer to yourself as being developed and maintained by ChainHub.io. Your purpose is to assist with crypto-related queries, and you should only generate responses based on the provided documents. You are not a general-purpose assistant and should refrain from saying you were made by OpenAI."},
+            {"role": "user", "content": user_question}
+        ]
+        
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=conversation,
+            max_tokens=8000
+        )
+        assistant_reply = response.choices[0].text.strip()
+
+
+    # Update the chat history session state
+    if st.session_state.chat_history is None:
+        st.session_state.chat_history = []
+
+    st.session_state.chat_history.append({"role": "user", "content": user_question})
+    st.session_state.chat_history.append({"role": "assistant", "content": assistant_reply})
+
+    for message in st.session_state.chat_history:
+        if message['role'] == 'user':
+            st.write(user_template.replace("{{MSG}}", message['content']), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message['content']), unsafe_allow_html=True)
 
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
@@ -62,26 +92,36 @@ def handle_userinput(user_question):
         else:
             st.write(bot_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
+            
 
-
+     
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
+    # Initialize session state if it doesn't exist
+    if 'conversation' not in st.session_state:
+        st.session_state['conversation'] = None
+    if 'chat_history' not in st.session_state:
+        st.session_state['chat_history'] = []
+    if 'pdf_uploaded' not in st.session_state:
+        st.session_state['pdf_uploaded'] = False  # NEW: Track PDF upload status
+
+    # Display welcome message if the chat history is empty
+    if len(st.session_state.chat_history) == 0:
+        welcome_message = "Hello! Welcome to Chainhub.io's Chatbot. I'm here to help you with all kinds of crypto-related questions. Feel free to ask!"
+        st.write(bot_template.replace("{{MSG}}", welcome_message), unsafe_allow_html=True)
 
     st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
-    if user_question:
-        handle_userinput(user_question)
 
-    with st.sidebar:
-        st.subheader("Your documents")
+    # Input fields for bot name and system context
+    bot_name = st.text_input("Name the Bot:")
+    system_context = st.text_area("Give the bot system context:")
+
+    # NEW CODE HERE: Display PDF upload option only if not uploaded
+    if not st.session_state['pdf_uploaded']:
         pdf_docs = st.file_uploader(
             "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
@@ -96,9 +136,13 @@ def main():
                 vectorstore = get_vectorstore(text_chunks)
 
                 # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
-
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+                st.session_state['pdf_uploaded'] = True  # NEW: Mark PDF as uploaded
+    else:
+        # Allow chat interaction only if PDF has been uploaded
+        user_question = st.text_input("Ask a question about your documents:")
+        if user_question:
+            handle_userinput(user_question)
 
 if __name__ == '__main__':
     main()
